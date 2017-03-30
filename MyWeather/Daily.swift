656ed9dc7fa911cf95f061e465efd4a851c891e6
lhassa8 <larstray@gmail.com>
2017-03-30 19:35:26 -0400

    work in progress, converting to Swift 3

---
+++ MyWeather/Daily.swift
@@ -0,0 +1,9 @@
+//
+//  Daily.swift
+//  MyWeather
+//
+//  Created by User on 5/31/16.
+//  Copyright © 2016 TheSitePass. All rights reserved.
+//
+
+import Foundation
---
+++ MyWeather/FormattedForecast.swift
@@ -0,0 +1,9 @@
+//
+//  FormattedForecast.swift
+//  MyWeather
+//
+//  Created by User on 5/31/16.
+//  Copyright © 2016 TheSitePass. All rights reserved.
+//
+
+import Foundation
